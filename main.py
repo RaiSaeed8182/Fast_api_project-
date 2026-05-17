168ba24a895c39bef1282b86948c71f0f8ecc394
RaiSaeed8182 <saeedrai81829@gmail.com>
2026-05-17 16:45:15 +0500

    add list and its types

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI 
+from pydantic import BaseModel 
 app=FastAPI()
 
 @app.get("/")
@@ -8,4 +9,35 @@ def read_root():
 def health_checker():
  return {"message":"helath","service":"medicare_pro_project"}
 
- 
+
+
+def calculate_consultation_fee(hours:float , rate_per_hour: float)-> float:
+    return hours*rate_per_hour
+
+
+print(calculate_consultation_fee(10.0,6.5))
+
+
+
+class patient:
+   def  __init__(self,name:str,age:int):
+    self.name=name 
+    self.age = age 
+
+p1=patient(name="Ali",age=5)
+p2=patient(name="Saeed",age=10)
+patients:list[patient]=[p1,p2]
+
+def get_patients()->list[patient]:
+    return patients
+
+
+class Doctor(BaseModel):
+    name:str 
+    age:int
+    fee:float 
+
+d1 = Doctor(name="saeed",age=13,fee=255.0)
+print(d1)
+d2 = Doctor(name=123,age="13",fee=255.0)
+print(d2)
